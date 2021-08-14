b0fcaa7b01eccfeabf0bd7a828d93b3ff38b20f4
Hugh Duong-Tran-Tien <hugh.dtt@gmail.com>
2021-08-14 14:28:18 +1000

    update to grid explanation

--- node/gridTravel.js
+++ node/gridTravel.js
@@ -1,11 +1,11 @@
-//Say that you are traveler on a 2D grid. You being in the top-left corner and your goal is to travel to the bottom-right corner. You can only move down or right.
-
-//In how many ways can you travel to the goal on a grid with dimensions m*n (implies rectangle)?
-
-//Write a function `gridTraveler(m,n)` that calculates this.
+/**
+ * Suppose that you are traveler on a 2D grid. You being in the top-left corner and your goal is to travel to the bottom-right corner. You can only move down or right.
+ * In how many ways can you travel to the goal on a grid with dimensions m*n (implies rectangle)?
+ * Write a function `gridTraveler(m,n)` that calculates this.
+ */
 
 /**
- *  gridTraveler(2,3) -> 3
+ *  gridTraveler(2,3) -> 3 
  *  1. right, right, down
  *  2. right, down, right
  *  3. down, right, right
@@ -18,6 +18,10 @@
  * 
  * Base Case -> gridTraveler(1,1) -> 1 ways
  *           -> gridTraveler(0,1) | gridTraveler(1,0) | gridTraveler(0,0) -> 0 ways
+ * 
+ * My next instinct is this is similiar to the fibonacci series where you are returning both the results from moving right or down (ie gridTravel(down) + gridTravel(right))
+ * Note that moving right or down simply reduces the grid size to either (m-1,n) or (m,n-1) respectively.
+ * I've memoised the recursive function below, but the logic remains the same.
  */
 
 //Memoised
