1e9bd543fa70f437a5173f51a33cd98121ed85da
Hugh Duong-Tran-Tien <hugh.dtt@gmail.com>
2021-08-31 21:40:24 +1000

    update

---
+++ node/eloquent/high-order-functions.js
@@ -0,0 +1,10 @@
+//Just a bit of analysis on one of the functions
+function noisy(f) {
+    return (...args) => {
+      console.log("calling with", args);
+      let result = f(...args);
+      console.log("called with", args, ", returned", result);
+      return result;
+    };
+  }
+  noisy(Math.min)(3, 2, 1);
